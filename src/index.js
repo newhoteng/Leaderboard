2765db93989cce99b9338dcc1891d0a1eadba1d4
Harriet Oteng <harriet.oteng@gmail.com>
2023-04-12 14:01:46 +0000

    Create game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+// Create new game
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'Oware',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+// const form = document.getElementById('form')
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault()
+
+//   const name = document.getElementById('name').value
+//   const score = document.getElementById('score').value
+
+//   // fetch post request
+// })
